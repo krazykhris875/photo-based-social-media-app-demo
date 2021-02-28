b7bced1006c73b9df6c4b620263db83d81f42812
Christopher Pickard <christopherpickard6@gmail.com>
2021-02-28 14:59:15 -0400

    Changed/added to bottom navigation and fragments so that there is now a home, search, videos, shop, and profile page.

---
+++ app/src/main/java/com/example/photobasedsocialmediaappdemo/ui/videos/VideosViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.photobasedsocialmediaappdemo.ui.videos
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class VideosViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is videos Fragment"
+    }
+    val text: LiveData<String> = _text
+}
